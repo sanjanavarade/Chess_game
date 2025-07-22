1fe8f9ee0d1bb8d4fc10bdc5366a3cb991be8fc1
sanjanavarade <127649474+sanjanavarade@users.noreply.github.com>
2025-07-23 00:03:34 +0530

    Chess Game Completed

---
+++ backend1/src/Game.ts
@@ -0,0 +1,87 @@
+import { WebSocket } from "ws";
+import { Chess } from "chess.js";
+import { GAME_OVER, INIT_GAME, MOVE } from "./messages";
+export class Game {
+  public player1: WebSocket;
+  public player2: WebSocket;
+  public board: Chess;
+  private startTime: Date;
+  private movecount=0;
+
+  constructor(player1: WebSocket, player2: WebSocket) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.board = new Chess();
+    this.startTime = new Date();
+    this.player1.send(
+      JSON.stringify({
+        type: INIT_GAME,
+        payload: {
+          color: "white",
+        },
+      })
+    );
+    this.player2.send(
+      JSON.stringify({
+        type: INIT_GAME,
+        payload: {
+          color: "black",
+        },
+      })
+    );
+  }
+  makeMove(
+    socket: WebSocket,
+    move: {
+      from: string;
+      to: string;
+    }
+  ) {
+    //validate the type of move using zod
+    if (this.movecount % 2 === 0 && socket !== this.player1) {
+      return;
+    }
+    if (this.movecount % 2 === 1 && socket !== this.player2) {
+      return;
+    }
+    console.log("did no early return");
+    try {
+      this.board.move(move);
+      
+    } catch (e) {
+      console.log(e);
+      return;
+    }
+    console.log("move succeded");
+    //check if the game is over
+    if (this.board.isGameOver()) {
+      this.player1.send(
+        JSON.stringify({
+          type: GAME_OVER,
+          payload: {
+            winner: this.board.turn() === "w" ? "black" : "white",
+          },
+        })
+      );
+      return;
+    }
+
+    if (this.movecount % 2 === 0) {
+      this.player2.send(
+        JSON.stringify({
+          type: MOVE,
+          payload: move,
+        })
+      );
+    } else {
+      this.player1.send(
+        JSON.stringify({
+          type: MOVE,
+          payload: move,
+        })
+      );
+    }
+    //send thr updtaetd board to both players
+    this.movecount++;
+  }
+}
